8ea5f9750e24ee4829993e333317fabb6be935fa
Milton <gmraivan@gmail.com>
2023-04-27 15:12:47 -0500

    Se añadio envireoment

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+const provider = new GoogleAuthProvider();
+const firebaseConfig = {
+  apiKey: "AIzaSyAxnw1Yx0XbkEfQMSAQNPwho7NZLnWNoNI",
+  authDomain: "fir-loginauth-5553c.firebaseapp.com",
+  projectId: "fir-loginauth-5553c",
+  storageBucket: "fir-loginauth-5553c.appspot.com",
+  messagingSenderId: "713488124585",
+  appId: "1:713488124585:web:b70b565d047973b424f7b8"
+};
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  ngOnInit(): void
+  {
+    initializeApp(firebaseConfig);
+  }
+  google_call() {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user.photoURL)
+        console.log(user.displayName)
+        console.log(credential?.idToken)
+
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
 }
