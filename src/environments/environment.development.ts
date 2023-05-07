4c51bf73c04ad9e43cc6f39ca91699d8e0b2fac8
Milton <gmraivan@gmail.com>
2023-05-07 15:41:15 -0500

    se añadio variable de entorno producccion

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-
-  url_api:'http://localhost:3000/api',
-  dataclientid:'358369041381-sq1lhru1k2u6mtph0bbkl5l5209hiua2.apps.googleusercontent.com'
+  url_api: 'http://localhost:3000/api',
+  dataclientid: '358369041381-sq1lhru1k2u6mtph0bbkl5l5209hiua2.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  url_api:'http://localhost:3000/api',
+  url_api:'https://backendphoenyx-production.up.railway.app',
   dataclientid:'358369041381-sq1lhru1k2u6mtph0bbkl5l5209hiua2.apps.googleusercontent.com'
 };
